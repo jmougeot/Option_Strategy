29eb29a503d0d9bce2945cea5f8f9c9e34518153
Jacques <jacques.mougeot@centrale-med.fr>
2025-10-20 12:56:00 +0200

    automatic commit

--- run.bat
+++ run.bat
@@ -5,19 +5,7 @@ REM ============================================================================
 
 cd /d "%~dp0"
 
-echo.
 echo Lancement de l'application...
-echo.
-
-REM Vérifier si l'environnement virtuel existe
-if not exist "venv\" (
-    echo ERREUR: Environnement virtuel non trouve !
-    echo.
-    echo Executez d'abord: install.bat
-    echo.
-    pause
-    exit /b 1
-)
 
 REM Activer l'environnement virtuel
 call venv\Scripts\activate.bat
